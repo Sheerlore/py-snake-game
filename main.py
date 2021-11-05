3d77d72f9f6b23ed5448a30ca32655ed0c2c8de6
uai <noema01@outlook.jp>
2021-11-05 16:18:18 +0900

    duke gopher add

--- main.py
+++ main.py
@@ -1,126 +1,230 @@
-import time
 import random
 import pygame
-from pygame import key
-from pygame.constants import BLEND_ALPHA_SDL2, K_DOWN, K_RIGHT, K_UP, K_LEFT
-from pygame.font import SysFont
 
-# Color variables
-white = (255, 255, 255)
-black = (0, 0, 0)
-blue = (0, 0, 255)
-red = (255, 0, 0)
+# CONSTANT variable ===========================
 # display
-disp_width = 800
-disp_height = 600
-font_size = 30
+DISPLAY_W = 800
+DISPLAY_H = 600
+BLOCK_SIZE = 40 
+BASIC_FONT_SIZE = 40
+SCORE_FONT_SIZE = 40
+TIMER_FONT_SIZE = 40
 # Snake
-snake_block = 20
-snake_speed = 15 
+SNAKE_SPEED = 15
 # food
-food_block = 20
-# pygame
-clock = pygame.time.Clock()
+FOOD_BLOCK_SIZE = 40 
+# Color variables
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
-def disp_score(score, disp, font_style):
-    value = font_style.render("Score: " + str(score), True, black)
-    disp.blit(value, [0, 0])
+# FUNCTION  =====================================
+def display_timer(disp, font_style, time, color = BLACK):
+    txt = font_style.render("time: " + time, True, color)
+    disp.blit(txt, [10, SCORE_FONT_SIZE])
 
-def message(msg, color, font_style, disp):
-    mesg = font_style.render(msg, True, color)
-    disp.blit(mesg, [disp_width / 6, disp_height / 3])
+def display_score(disp, font_style, score, color = BLACK):
+    value = font_style.render("score: " + str(score), True, color)
+    disp.blit(value, [10, 0])
 
-def our_snake(snake_block, snake_list, disp):
-    for x in snake_list:
-        pygame.draw.rect(disp, black, [x[0], x[1], snake_block, snake_block])
+def display_message(disp, font_style, text, position = [0, 0], color = BLACK):
+    msg = font_style.render(text, True, color)
+    disp.blit(msg, position)
 
-def main():
-    pygame.init()
+def draw_snake(disp, snake_list,img_head, img,size = BLOCK_SIZE):
+    for x in snake_list[:-1]:
+        # pygame.draw.rect(disp, BLACK, [x[0], x[1], size, size])
+        disp.blit(img, [x[0], x[1]])
+    disp.blit(img_head, [snake_list[-1][0], snake_list[-1][1]])
 
-    disp = pygame.display.set_mode(([disp_width, disp_height]))
-    pygame.display.set_caption("Snake game")
+def draw_blood(disp, blood_list,img):
+    for x in blood_list:
+        disp.blit(img, [x[0], x[1]])
 
-    x1 = disp_width / 2
-    y1 = disp_height / 2
+def draw_food_img(disp, img, position = [0, 0]):
+    disp.blit(img, position)
+
+def is_hit_snake_and_food(snake_x, snake_y, food_x, food_y, snake_size, food_size):
+    if (((food_x <= snake_x) and (snake_x <= (food_x + food_size))) and \
+        ((food_y <= snake_y) and (snake_y <= (food_y + food_size)))) or \
+        (((food_x <= (snake_x + snake_size)) and ((snake_x + snake_size) <= (food_x + food_size))) and \
+        ((food_y <= (snake_y + snake_size)) and ((snake_y + snake_size) <= (food_y + food_size)))) or \
+        (((food_x <= snake_x) and (snake_x <= (food_x + food_size))) and \
+        ((food_y <= (snake_y + snake_size)) and ((snake_y + snake_size) <= (food_y + food_size)))) or \
+        (((food_x <= (snake_x + snake_size)) and ((snake_x + snake_size) <= (food_x + food_size))) and \
+        ((food_y <= snake_y) and (snake_y <= (food_y + food_size)))):
+        return True
+    else:
+        return False
+
+def calc_snake_pos_diff_by_key(key, move_amount):
     x1_change = 0
     y1_change = 0
-    food_x = round(random.randrange(0, disp_width - snake_block) / 10.0) * 10.0
-    food_y = round(random.randrange(0, disp_height- snake_block) / 10.0) * 10.0
+    if key == pygame.K_LEFT: # ←
+        x1_change = -move_amount
+        y1_change = 0
+    elif key == pygame.K_RIGHT: # →
+        x1_change = move_amount 
+        y1_change = 0
+    elif key == pygame.K_UP: # ↑
+        x1_change = 0
+        y1_change = -move_amount
+    elif key == pygame.K_DOWN: # ↓
+        x1_change = 0
+        y1_change = move_amount 
+    return (x1_change, y1_change)
 
-    font_style = pygame.font.SysFont(None, font_size)
-    font_style_score = pygame.font.SysFont(None, font_size)
 
+def game_loop():
+    # /=========== game init ===============\
+    pygame.init()
+    disp = pygame.display.set_mode(([DISPLAY_W, DISPLAY_H]))
+    pygame.display.set_caption("Snake game Duke VS Gopher")
+    # init snake position
+    snake_x = DISPLAY_W / 2
+    snake_y = DISPLAY_H / 2
+    snake_dx = 0
+    snake_dy = 0
+    # init food position
+    food_x = round(random.randrange(0, DISPLAY_W - BLOCK_SIZE) / 10.0) * 10.0
+    food_y = round(random.randrange(0, DISPLAY_H - BLOCK_SIZE) / 10.0) * 10.0
+    # init font style
+    basic_font_style = pygame.font.SysFont(None, BASIC_FONT_SIZE)
+    socre_font_style = pygame.font.SysFont(None, SCORE_FONT_SIZE)
+    timer_font_style = pygame.font.SysFont(None, TIMER_FONT_SIZE)
+    # snake conf 
     snake_list = []
-    Length_of_snake = 1
-
+    length_of_snake = 1
+    # clock time in pygame
+    clock = pygame.time.Clock()
+    # blood
+    blood_list = []
+    # game flag 
     game_over = False
     game_close = False
+    close_out_display = False
+    close_time_up = False
+    close_hurt_self = False
+    # timer
+    counter = 60
+    text = str(counter)
+    pygame.time.set_timer(pygame.USEREVENT, 1000)
+
+    # \=========== game init ===============/
+
+    # /=========== game main ===============\
     while not game_over:
+        # img
+        gopher_img = pygame.image.load('./gopher.png')
+        gopher_d_img = pygame.image.load('./gopher_d.png')
+        duke_img = pygame.image.load('./duke.png')
+        blood_img = pygame.image.load('./blood.png')
+        gopher_img = pygame.transform.scale(gopher_img, (FOOD_BLOCK_SIZE, FOOD_BLOCK_SIZE))
+        gopher_d_img = pygame.transform.scale(gopher_d_img, (FOOD_BLOCK_SIZE, FOOD_BLOCK_SIZE))
+        duke_img = pygame.transform.scale(duke_img, (BLOCK_SIZE, BLOCK_SIZE))
+        blood_img = pygame.transform.scale(blood_img, (BLOCK_SIZE, BLOCK_SIZE))
+
+        # /-------- game close process -----------\
         while game_close == True:
-            disp.fill(white)
-            message("You Lost! Press Q-Quit or C-Play Again", black, font_style, disp)
-            disp_score(Length_of_snake - 1, disp, font_style_score)
-            pygame.display.update()
+            disp.fill(WHITE)
+            if close_hurt_self or close_out_display:
+                display_message(disp,
+                                basic_font_style,
+                                "You Lost!",
+                                position=[DISPLAY_W / 4, DISPLAY_H / 3])
+            elif close_time_up:
+                display_message(disp,
+                                basic_font_style,
+                                "time up!",
+                                position=[DISPLAY_W / 4, DISPLAY_H / 3])
+
+            display_message(disp,
+                            basic_font_style,
+                            "score: " + str(length_of_snake - 1),
+                            position=[DISPLAY_W / 4, DISPLAY_H / 3 + BASIC_FONT_SIZE])
+            display_message(disp,
+                            basic_font_style,
+                            "Press [Q] Quit [C] Play Again",
+                            position=[DISPLAY_W / 4, DISPLAY_H / 3 + BASIC_FONT_SIZE*2])
+            
+            pygame.display.update() 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.QUIT:
+                        game_over = True
+                        game_close = False
                     if event.key == pygame.K_q:
                         game_over = True
                         game_close = False
                     if event.key == pygame.K_c:
-                        main()
+                        game_loop()
+        # \-------- game close process -----------/
 
+
+        # /-------- game main process -----------\
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game_close = True
-            
+                game_over = True
             if event.type == pygame.KEYDOWN:
-                key = event.key
-                if key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif key == pygame.K_RIGHT:
-                    x1_change = snake_block 
-                    y1_change = 0
-                elif key == pygame.K_UP:
-                    x1_change = 0
-                    y1_change = -snake_block
-                elif key == pygame.K_DOWN:
-                    x1_change = 0
-                    y1_change = snake_block 
+                snake_dx, snake_dy = calc_snake_pos_diff_by_key(event.key, BLOCK_SIZE)
+            if event.type == pygame.USEREVENT:
+                counter -= 1
+                text = str(counter)
         
-        if x1 >= disp_width or x1 < 0 or y1 >= disp_height or y1 < 0:
+        if counter < 0:
             game_close = True
+            close_time_up = True
+        
+        
+        # go display outside
+        if snake_x < 0 or DISPLAY_W <= snake_x or \
+            snake_y < 0 or DISPLAY_H <= snake_y:
+            game_close = True
+            close_out_display = True
 
-        x1 += x1_change
-        y1 += y1_change
-        disp.fill(white)
-        pygame.draw.rect(disp, blue, [food_x, food_y, food_block, food_block])
-        snake_Head = []
-        snake_Head.append(x1)
-        snake_Head.append(y1)
+        # snake pos change
+        snake_x += snake_dx
+        snake_y += snake_dy
+
+        # food draw
+        disp.fill(WHITE)
+        # pygame.draw.rect(disp, BLUE, [food_x, food_y, FOOD_BLOCK_SIZE, FOOD_BLOCK_SIZE])
+        draw_blood(disp, blood_list, blood_img)
+        draw_food_img(disp, gopher_img, [food_x, food_y])
+
+        snake_Head = [snake_x, snake_y]
         snake_list.append(snake_Head)
-        if len(snake_list) > Length_of_snake:
+        if len(snake_list) > length_of_snake:
             del snake_list[0]
         
         for x in snake_list[:-1]:
             if x == snake_Head:
                 game_close = True
-        our_snake(snake_block, snake_list, disp)
-        disp_score(Length_of_snake - 1, disp, font_style_score)
+                close_hurt_self = True
 
+        draw_snake(disp, snake_list, duke_img, gopher_d_img)
+        display_score(disp, socre_font_style, length_of_snake - 1)
+        display_timer(disp, timer_font_style, text)
         pygame.display.update()
 
-        if x1 == food_x and y1 == food_y:
-            food_x = round(random.randrange(0, disp_width - snake_block) / 10.0) * 10.0
-            food_y = round(random.randrange(0, disp_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 1
-
-        clock.tick(snake_speed)
+        if is_hit_snake_and_food(snake_x, snake_y, food_x, food_y, BLOCK_SIZE, BLOCK_SIZE):
+            blood_list.append([food_x, food_y])
+            food_x = round(random.randrange(0, DISPLAY_W - BLOCK_SIZE) / 10.0) * 10.0
+            food_y = round(random.randrange(0, DISPLAY_H - BLOCK_SIZE) / 10.0) * 10.0
+            length_of_snake += 1
+        
+        clock.tick(SNAKE_SPEED)
+        
+        # \-------- game main process -----------/
 
     pygame.quit()
+    quit()
+    # \=========== game main ===============/
 
 
 if __name__ == "__main__":
     print("===Start Pygame [Snake Game]===")
-    main()
-    quit()
+    game_loop()
